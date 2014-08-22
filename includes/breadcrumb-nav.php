b1e40cc1a7f9f846e5a5ea3eff6a28a92c766f21
Eric Andrew Lewis <eric.andrew.lewis@gmail.com>
2014-08-21 22:18:04 -0400

    Remove unnecessary files.

--- includes/breadcrumb-nav.php
+++
@@ -1,5 +0,0 @@
-<div class="row">
-  <div id="page-title" class="span6">
-    <h4><a href="<?php echo 'http://'.$_SERVER['HTTP_HOST']; ?>"><i class="fa fa-home"></i></a> /projects/example/ <i class="fileupload fa fa-cloud-upload"></i> <i class="btn-slideshow fa fa-bullseye"></i></h4>
-  </div>
-</div>
--- includes/empty-dir.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-  echo <<<EOF
-  <div class="empty">
-    <p>empty</p>
-  </div>
-EOF;
-
-?>
